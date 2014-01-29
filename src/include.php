33e53c9eaad4d151255ce55dde7b011bcfbcdf82
Vir4030 <erick@hydratedearth.com>
2014-01-28 22:33:33 -0600

    autoloader

--- src/include.php
+++ src/include.php
@@ -1,12 +1,22 @@
 <?php
-require_once 'norm/DBConnection.php';
-require_once 'norm/DBEntity.php';
-require_once 'norm/DBField.php';
-require_once 'norm/DBForeignKey.php';
-require_once 'norm/DBLog.php';
-require_once 'norm/DBQuery.php';
-require_once 'norm/DBStore.php';
-require_once 'norm/db/MSSQLConnection.php';
-require_once 'norm/db/MySQLConnection.php';
+$NORM_CLASS_RACK = array(
+		'DBConnection'		=>	'norm/DBConnection.php',
+		'DBEntity'			=>	'norm/DBEntity.php',
+		'DBField'			=>	'norm/DBField.php',
+		'DBForeignKey'		=>	'norm/DBForeignKey.php',
+		'DBLog'				=>	'norm/DBLog.php',
+		'DBQuery'			=>	'norm/DBQuery.php',
+		'DBStore'			=>	'norm/DBStore.php',
+		'MSSQLConnection'	=>	'norm/db/MSSQLConnection.php',
+		'MySQLConnection'	=>	'norm/db/MySQLConnection.php',
+);
+
+function autoload_norm($class) {
+	global $NORM_CLASS_RACK;
+	if (isset($NORM_CLASS_RACK[$class]))
+		include $NORM_CLASS_RACK[$class];
+}
+
+spl_autoload_register('autoload_norm');
 
 date_default_timezone_set('UTC');
